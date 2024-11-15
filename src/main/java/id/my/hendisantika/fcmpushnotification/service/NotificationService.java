9e9ff4f279df018e44a6e05ef00f0b185e453c7d
hendisantika <hendisantika@yahoo.co.id>
2024-11-16 06:17:07 +0700

    feat: Add NotificationService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/fcmpushnotification/service/NotificationService.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.fcmpushnotification.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-fcm-push-notification
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/11/24
+ * Time: 06.16
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+
+    private final SimpMessagingTemplate template;
+}
