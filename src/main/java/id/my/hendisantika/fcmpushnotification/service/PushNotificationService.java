e2f4ee90a34ec50b017ce6e57bc9d6508eaa6143
hendisantika <hendisantika@yahoo.co.id>
2024-11-15 07:23:50 +0700

    Add PushNotificationService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/fcmpushnotification/service/PushNotificationService.java
@@ -0,0 +1,32 @@
+package id.my.hendisantika.fcmpushnotification.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-fcm-push-notification
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/11/24
+ * Time: 07.23
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class PushNotificationService {
+
+    @Value("#{${app.notifications.defaults}}")
+    private Map<String, String> defaults;
+
+    private final Logger logger = LoggerFactory.getLogger(PushNotificationService.class);
+    private final FCMService fcmService;
+
+    public PushNotificationService(FCMService fcmService) {
+        this.fcmService = fcmService;
+    }
+}
