68f3915594460285bc8cf308834eaa6970b047c5
hendisantika <hendisantika@yahoo.co.id>
2024-11-15 07:27:13 +0700

    Add FCMService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/fcmpushnotification/firebase/FCMService.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.fcmpushnotification.firebase;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-fcm-push-notification
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/11/24
+ * Time: 07.26
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+public class FCMService {
+
+    private final Logger logger = LoggerFactory.getLogger(FCMService.class);
+}
