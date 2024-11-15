98d3a13f90a089ec65c6b69c7a6fa966e839f881
hendisantika <hendisantika@yahoo.co.id>
2024-11-15 07:26:40 +0700

    Add initialize in FCMInitializer🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/fcmpushnotification/firebase/FCMInitializer.java
+++ src/main/java/id/my/hendisantika/fcmpushnotification/firebase/FCMInitializer.java
@@ -1,8 +1,15 @@
 package id.my.hendisantika.fcmpushnotification.firebase;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +28,17 @@ public class FCMInitializer {
     @Value("${app.firebase-configuration-file}")
     private String firebaseConfigPath;
 
+    @PostConstruct
+    public void initialize() {
+        try {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())).build();
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                log.info("Firebase application has been initialized");
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
 }
