3e46a324fe86d9dcd89d525ebd8fc3d338f83434
hendisantika <hendisantika@yahoo.co.id>
2024-11-15 07:41:25 +0700

    fix: Integration Test🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/fcmpushnotification/SpringBootFcmPushNotificationApplicationTests.java
+++ src/test/java/id/my/hendisantika/fcmpushnotification/SpringBootFcmPushNotificationApplicationTests.java
@@ -1,13 +1,43 @@
 package id.my.hendisantika.fcmpushnotification;
 
+import com.google.firebase.FirebaseApp;
+import id.my.hendisantika.fcmpushnotification.firebase.FCMInitializer;
+import id.my.hendisantika.fcmpushnotification.firebase.FCMService;
+import id.my.hendisantika.fcmpushnotification.model.PushNotificationRequest;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 class SpringBootFcmPushNotificationApplicationTests {
 
+    @Mock
+    FCMInitializer fcmInitializer;
+
+    @Mock
+    FCMService fcmClient;
+
     @Test
-    void contextLoads() {
+    public void firebaseAppInitializationTest() {
+        fcmInitializer.initialize();
+        System.out.println(FirebaseApp.getApps());
+        assertFalse(FirebaseApp.getApps().isEmpty());
     }
 
+    @Test
+    public void sendTestMessage() {
+        PushNotificationRequest request = new PushNotificationRequest("hello", "testMessage", "testTopic");
+        Map<String, String> pushData = new HashMap<>();
+        try {
+            fcmClient.sendMessage(pushData, request);
+        } catch (Exception e) {
+            fail();
+        }
+    }
 }
